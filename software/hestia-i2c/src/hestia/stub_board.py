c6e3afa40a70abc9ff8d19b4e80aa32a896088ab
Matt Ryall <mryall@mawsonrovers.com>
2022-12-09 14:18:49 +1100

    hestia-i2c: more realistic test data

--- software/hestia-i2c/src/hestia/stub_board.py
+++ software/hestia-i2c/src/hestia/stub_board.py
@@ -1,5 +1,7 @@
 import math
-from typing import Dict
+from datetime import datetime, timedelta
+from random import random
+from typing import Dict, Optional
 
 from hestia import Hestia
 from hestia.board import Sensor
@@ -34,12 +36,23 @@ class StubHestia(Hestia):
         super().__init__()
         self.heater_enabled: bool = False
         self.heater_pwm: int = 50
+        self.heater_started: Optional[datetime] = None
 
     def read_sensor_values(self) -> Dict[Sensor, float]:
-        return {s: _stub_values.get(s.id, math.nan) for s in self.sensors}
+        return {s: self.read_sensor(s.id) for s in self.sensors}
+
+    def read_sensor(self, sensor_id):
+        base_val = _stub_values.get(sensor_id, math.nan) + random() * 5 - 2
+        if self.heater_enabled:
+            heating_duration: timedelta = datetime.now() - self.heater_started
+            max_temp: float = 50 + self.heater_pwm / 4
+            return round(min(base_val + self.heater_pwm / 50 * heating_duration.total_seconds(),
+                             max_temp), 4)
+        else:
+            return round(base_val, 4)
 
     def read_center_temp(self) -> float:
-        return _stub_values['TH1']
+        return self.read_sensor('TH1')
 
     def is_heater_enabled(self) -> bool:
         return self.heater_enabled
@@ -52,6 +65,7 @@ class StubHestia(Hestia):
 
     def enable_heater(self):
         self.heater_enabled = True
+        self.heater_started = datetime.now()
 
     def disable_heater(self):
         self.heater_enabled = False
