e43f03fb40383ba05f4b5b3e09277a59b4123e9d
Matt Ryall <mryall@mawsonrovers.com>
2023-01-18 14:35:40 +1100

    hestia-i2c: move sensor test to test_sensors.py

--- software/hestia-i2c/tests/test_heater.py
+++ software/hestia-i2c/tests/test_heater.py
@@ -1,5 +1,4 @@
 import logging
-import math
 from time import sleep
 
 from hestia import Hestia
@@ -9,23 +8,17 @@ log = logging.getLogger(__name__)
 hestia = Hestia()
 
 
-def test_sensors():
-    for sensor, temp in hestia.read_sensor_values().items():
-        log.info('%s (0x%02x) => %.2f', sensor.label, sensor.addr, temp)
-        assert 10.0 <= temp <= 80.0 or math.isnan(temp)
-
-
 def test_heater_works():
     start_temp = hestia.read_center_temp()
     log.info('Start temp: %.2f', start_temp)
     assert not hestia.is_heater_enabled(), "Heater should not be enabled before"
     with hestia.heating():
         assert hestia.is_heater_enabled(), "Heater should be enabled"
-        sleep(30)
+        sleep(10)
     finish_temp = hestia.read_center_temp()
     assert not hestia.is_heater_enabled(), "Heater should not be enabled after"
     log.info('Finish temp: %.2f', finish_temp)
-    assert 5.0 < (finish_temp - start_temp) <= 40.0, "Should increase temperature a bit"
+    assert 3.0 < (finish_temp - start_temp) <= 20.0, "Should increase temperature a little"
 
 
 def test_heater_full_power():
@@ -35,4 +28,4 @@ def test_heater_full_power():
         sleep(10)
     finish_temp = hestia.read_center_temp()
     log.info('Finish temp: %.2f', finish_temp)
-    assert 5.0 < (finish_temp - start_temp) <= 40.0, "Should increase temperature a bit"
+    assert 5.0 < (finish_temp - start_temp) <= 40.0, "Should increase temperature some more"
