91a8458f50b334b9da4d64220f703b9131e81b56
Matt Ryall <mryall@mawsonrovers.com>
2023-06-14 23:53:58 +1000

    #30: better directory structure on beagle
    - moved everything under ~/uts
    - got hot-reload working
    - added nginx config
    - tested on a bare BBB config

--- hestia-sat/upload.sh
+++ hestia-sat/upload.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-DEST=$1
-FILE=$2
-if [ -z "$DEST" ] || [ -z "$FILE" ]; then
-  echo "Usage: $0 host:path/ file"
+FILE=$1
+if [ -z "$FILE" ]; then
+  echo "Usage: $0 file"
   exit 1
 fi
 PATH=$(dirname "$FILE")
 
+HOST=beagle  # map this to the actual host name in your ~/.ssh/config
+DEST=/home/debian/uts
+
 set -x # echo commands from now on
-/usr/bin/rsync -utvz -e /usr/bin/ssh "$PATH"/{uts-cli,uts-log,uts-web} "$DEST"
+/usr/bin/rsync -utvz -e /usr/bin/ssh "$PATH"/{uts-cli,uts-log,uts-web} "$HOST:$DEST/bin/"
+/usr/bin/rsync -utvzr -e /usr/bin/ssh ./{nginx,systemd} "$HOST:$DEST/"
