c30582957bc03b58f8ff9910dbd9999615a97929
Matt Ryall <mryall@mawsonrovers.com>
2023-06-14 15:06:26 +1000

    #28: ignore dangling labels in ERC, download 3D models, fix more KiCAD 6->7 config

--- scripts/kibot-build.sh
+++ scripts/kibot-build.sh
@@ -18,8 +18,10 @@ pushd $PCB_PATH  # do this first, so we fail if we're in the wrong spot
 echo "[INFO] Deleting old build files"
 [ -d "$BUILD_PATH" ] && rm -r "$BUILD_PATH"
 
-echo "[INFO] Installing 3D models"
-/usr/bin/kicad_3d_install.sh
+if [ -z "$KICAD7_3DMODEL_DIR" ]; then
+    echo "[INFO] Installing 3D models"
+    /usr/bin/kicad_3d_install.sh
+fi
 
 # Ensure we set KICAD7_3DMODEL_DIR to avoid lots of build errors
 export KICAD7_3DMODEL_DIR=${KICAD7_3DMODEL_DIR:-'/usr/share/kicad/3dmodels'}
@@ -28,7 +30,7 @@ echo "[INFO] KICAD7_3DMODEL_DIR set to $KICAD7_3DMODEL_DIR"
 CONFIG=hestia.kibot.yml
 echo "[INFO] Starting kibot with config: $CONFIG"
 kibot -c "$CONFIG" -b hestia.kicad_pcb -e hestia.kicad_sch \
-          -d "$BUILD_PATH"
+      -d "$BUILD_PATH"
 popd
 
 echo "[INFO] Finished build script $0"
--- scripts/mac-kibot.sh
+++ scripts/mac-kibot.sh
@@ -9,7 +9,7 @@ docker run --platform=linux/amd64 -i \
     -v $HOME/src/mawson/hestia:/local/src \
     -v /Applications/KiCad/KiCad.app/Contents/SharedSupport:/local/kicad \
     -v $HOME/Library/Preferences/kicad:/root/.config/kicad \
-    -e 'KICAD6_3DMODEL_DIR=/local/kicad/3dmodels' \
+    -e 'KICAD7_3DMODEL_DIR=/local/kicad/3dmodels' \
     -w /local/src \
     -t ghcr.io/inti-cmnb/kicad7_auto:1.6.1 \
     "$COMMAND"
