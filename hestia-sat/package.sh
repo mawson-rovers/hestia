c700299f0182aac3c738399f929c45d34cf6f7a8
Matt Ryall <mryall@mawsonrovers.com>
2023-09-27 12:49:35 +1000

    #65: don't include config in releases

--- hestia-sat/package.sh
+++ hestia-sat/package.sh
@@ -12,11 +12,10 @@ ARCHIVE="target/hestia_sat-$VERSION-$TARGET.tar.gz"
 
 set -x # echo commands from now on
 rm -r $OUT
-mkdir -p $OUT/{bin,www,nginx,systemd}
+mkdir -p $OUT/{bin,www}
 
 cp -p "target/$TARGET/debug/"uts-{cli,log,web,run,update} $OUT/bin/
 cp -p ./uts-programs.toml $OUT/uts-programs-example.toml
-cp -Rp ./{nginx,systemd} $OUT/
 cp -Rp ../hestia-static-dash/* $OUT/www/
 
 tar zcf "$ARCHIVE" --strip-components 2 --exclude '.DS_Store' $OUT
