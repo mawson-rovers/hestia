e6cfe5814e6784accff5dc0fda715cb40ae052ce
Matt Ryall <mryall@mawsonrovers.com>
2024-01-31 16:22:09 +1100

    #81 Add kubos app manifests
    Add kubos app manifests to kubos directory
    Symlink binaries into these directories in the package
    Update package names to reflect project name
    Create two packages: "earth" for UTS testing, "space" is just what's needed for WS-1
    Remove platform from package name to shorten filename for operational simplicity

--- hestia-sat/package.sh
+++ hestia-sat/package.sh
@@ -6,11 +6,16 @@ if [ -z "$VERSION" ]; then
   exit 1
 fi
 
-OUT="target/release"
+OUT="target/package"
 TARGET="arm-unknown-linux-gnueabihf"
-ARCHIVE="target/hestia_sat-$VERSION-$TARGET.tar.gz"
+EARTH_ARCHIVE="target/uts-matilda-$VERSION-earth.tar.gz"
+SPACE_ARCHIVE="target/uts-matilda-$VERSION-space.tar.gz"
+
+export COPYFILE_DISABLE=1  # On Mac, don't add ._ resource fork files in tarballs
 
 set -x # echo commands from now on
+
+echo "Building test archive"
 rm -r $OUT
 mkdir -p $OUT/{bin,www,programs}
 
@@ -18,5 +23,17 @@ cp -p "target/$TARGET/release/"uts-{cli,log,web,run,update} $OUT/bin/
 cp -p programs/*.toml $OUT/programs/
 cp -Rp ../hestia-static-dash/* $OUT/www/
 
-tar zcf "$ARCHIVE" --strip-components 2 --exclude '.DS_Store' $OUT
+tar zcf "$EARTH_ARCHIVE" --no-xattrs --strip-components 2 --exclude '.DS_Store' $OUT
 
+echo "Building prod archive"
+rm -r $OUT
+mkdir -p $OUT/{bin,programs,kubos}
+
+cp -p "target/$TARGET/release/"uts-{cli,log,run} $OUT/bin/
+cp -p programs/*.toml $OUT/programs/
+cp -R kubos/* $OUT/kubos/
+ln -s "../../bin/uts-cli" $OUT/kubos/uts-cli/
+ln -s "../../bin/uts-log" $OUT/kubos/uts-log/
+ln -s "../../bin/uts-run" $OUT/kubos/uts-run/
+
+tar zcf "$SPACE_ARCHIVE" --no-xattrs --strip-components 2 --exclude '.DS_Store' $OUT
