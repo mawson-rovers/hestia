f329d29b6783349d0ce6c5f68f196a7b39f5d35b
Christopher Owen <cowen@mawsonrovers.com>
2024-02-13 13:31:57 +1100

    Log filesystem problems in zipper instead of panic #83

--- hestia-sat/src/zipper.rs
+++ hestia-sat/src/zipper.rs
@@ -7,7 +7,7 @@ use std::time::SystemTime;
 use flate2::Compression;
 use flate2::write::GzEncoder;
 use glob::glob;
-use log::info;
+use log::{error, info};
 
 use crate::payload::Config;
 
@@ -24,28 +24,57 @@ pub fn zip_logs(config: &Config) {
     }
 }
 
-fn zip_file(in_file: PathBuf, download_path: &PathBuf) {
-    let mut out_file = download_path.clone();
-    out_file.push(in_file.file_name().unwrap());
-    out_file.set_extension("csv.gz");
-    if out_file.exists() && mtime(&out_file) >= mtime(&in_file) {
-        // ignore files we've already compressed
-        return
+fn zip_file(in_path: PathBuf, download_path: &PathBuf) {
+    let mut out_path = download_path.clone();
+    out_path.push(in_path.file_name().unwrap());
+    out_path.set_extension("csv.gz");
+
+    if out_path.exists() {
+        let out_mtime = match mtime(&out_path) {
+            Ok(mtime) => mtime,
+            Err(err) => {
+                error!("Could not read mtime of {}: {}", out_path.display(), err);
+                return;
+            }
+        };
+        let in_mtime = match mtime(&in_path) {
+            Ok(mtime) => mtime,
+            Err(err) => {
+                error!("Could not read mtime of {}: {}", in_path.display(), err);
+                return;
+            }
+        };
+        if out_mtime >= in_mtime {
+            // ignore files we've already compressed
+            info!("Not compressing {} as it has already been compressed", in_path.display());
+            return
+        }
     }
 
-    if let Ok(in_file) = File::open(in_file) {
+    if let Ok(in_file) = File::open(&in_path) {
         let mut input = BufReader::new(in_file);
-        let output = File::create(out_file).expect("Should be able to write output file");
+        let output = match File::create(&out_path) {
+            Ok(file) => file,
+            Err(err) => {
+                error!("Could not create file {}: {}", out_path.display(), err);
+                return;
+            }
+        };
         let mut output = BufWriter::new(GzEncoder::new(output, Compression::fast()));
-        let _ = io::copy(&mut input, &mut output);
+        if let Err(err) = io::copy(&mut input, &mut output) {
+            error!("Could not write compressed stream for {}: {}", in_path.display(), err);
+            return
+        }
     }
 }
 
-fn mtime(file: &Path) -> u64 {
+fn mtime(file: &Path) ->  io::Result<u64> {
     assert!(file.exists(), "file should exist: {}", file.display());
-    // why is this so hard?! ¯\_(ツ)_/¯
-    file.metadata().unwrap()
-        .modified().unwrap()
-        .duration_since(SystemTime::UNIX_EPOCH).unwrap()
-        .as_secs()
+    let modified =
+        file.metadata()?
+        .modified()?;
+    match modified.duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(duration) => Ok(duration.as_secs()),
+        Err(err) => Err(io::Error::other(err.to_string()))
+    }
 }
